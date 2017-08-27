6cda61140d4facbbd0dae2018deffe79a99403ac
Dave Vera <dvera2@gmail.com>
2017-08-26 17:28:35 -0700

    Added framework for wire cutter minigame

---
+++ Assets/Scripts/WireCutterMinigame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WireCutterMinigame : MonoBehaviour {
+
+
+
+    public RectTransform WireCutter;
+
+    public RectTransform[] Positions;
+
+	// Use this for initialization
+	void Start () {
+        SetPosition(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        if (x < 0)
+        {
+            SetPosition(0);
+
+        } else if( x > 0 )
+        {
+            SetPosition(1);
+        }
+
+	}
+
+    public void SetPosition( int index )
+    {
+        WireCutter.anchoredPosition = Positions [index].anchoredPosition;
+    }
+}
